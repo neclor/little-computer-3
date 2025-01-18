85e3551cf9ea770a4624a79d10f7b026875797d9
neclor <neclordev@gmail.com>
2025-01-18 09:41:23 +0100

    a

--- src/lc3.rs
+++ src/lc3.rs
@@ -1,20 +1,26 @@
 
 
-const R0: usize = 0;
-const R1: usize = 1;
-const R2: usize = 2;
-const R3: usize = 3;
-const R4: usize = 4;
-const R5: usize = 5;
-const R6: usize = 6;
-const R7: usize = 7;
-const PC: usize = 8;
-const CC: usize = 9;
+macro_rules! get_r0 {($instr: expr) => {(($instr >> 9 & 0b111) as usize)};}
+macro_rules! get_r1 {($instr: expr) => {(($instr >> 6 & 0b111) as usize)};}
+macro_rules! get_r2 {($instr: expr) => {(($instr & 0b111) as usize)};}
+macro_rules! get_imm5_flag {($instr: expr) => {($instr >> 5 & 1 == 1)};}
+macro_rules! sign_extend {
+	($a: expr, $bit_count: expr) => {
+		(
+			if $a >> ($bit_count - 1) & 1 == 1 {
+				$a | (0xFFFF << $bit_count)
+			}
+			else {
+				$a & ((1 << $bit_count) - 1)
+			}
+		)
+	};
+}
 
 
-const N: u16 = 0b100;
-const Z: u16 = 0b010;
 const P: u16 = 0b001;
+const Z: u16 = 0b010;
+const N: u16 = 0b100;
 
 
 const ADD: u16 = 0b0001;
@@ -37,61 +43,68 @@ const TRAP: u16 = 0b1111;
 const RESERVED: u16 = 0b1101;
 
 
-const REG_COUNT: usize = 10;
-const MEMORY_SIZE: usize = u16::MAX as usize;
+const TRAP_GETC: usize = 0x20;
+const TRAP_OUT: usize = 0x21;
+const TRAP_PUTS: usize = 0x22;
+const TRAP_IN: usize = 0x23;
+const TRAP_PUTSP: usize = 0x24;
+const TRAP_HALT: usize = 0x25;
+
+
+/*
+const KBSR: usize = 0xFE00;
+const KBDR: usize = 0xFE02;
+const DSR: usize = 0xFE04;
+const DDR: usize = 0xFE06;
+const MCR: usize = 0xFFFE;
+*/
+
+
+const MEMORY_SIZE: usize = 1 << 16;
+const REG_COUNT: usize = 8;
 const PC_START: u16 = 0x3000;
 
 
-struct LC3 {
+pub struct LC3 {
+	mem: [u16; MEMORY_SIZE],
 	reg: [u16; REG_COUNT],
-	mem: [u16; MEMORY_SIZE]
+	pc: u16,
+	cc: u16
 }
 
 
 impl LC3 {
-	const PC_START: u16 = 0x3000;
-
-
 	pub fn new() -> Self {
-		let mut lc3: Self = Self {reg: [0; REG_COUNT], mem: [0; MEMORY_SIZE]};
-		lc3.reg[PC] = Self::PC_START;
-		lc3.reg[CC] = Z;
-		return lc3;
+		Self {
+			mem: [0; MEMORY_SIZE],
+			reg: [0; REG_COUNT],
+			pc: PC_START,
+			cc: Z
+		}
 	}
 
 
 	pub fn update(&mut self) -> Result<(), ()> {
-		let instruction: u16 = self.read_mem(self.reg[PC]);
-		self.reg[PC] += 1;
-		return self.execute(instruction);
-	}
-
-
-	fn read_mem(&self, address: u16) -> u16 {
-		return self.mem[address as usize];
-	}
-
-
-	fn execute(&mut self, instruction: u16) -> Result<(), ()> {
-		let opcode: u16 = instruction >> 12 & 0b1111;
+		let instr: u16 = self.mem[self.pc as usize];
+		self.pc += 1;
+		let opcode: u16 = instr >> 12 & 0xF;
 
 		match opcode {
-			ADD => self.add(instruction),
-			AND => self.and(instruction),
-
-			BR => self.add(instruction),
-			JMP => self.add(instruction),
-			JSR => self.add(instruction),
-			LD => self.add(instruction),
-			LDI => self.add(instruction),
-			LDR => self.add(instruction),
-			LEA => self.add(instruction),
-			NOT => self.add(instruction),
-			RTI => self.add(instruction),
-			ST => self.add(instruction),
-			STI => self.add(instruction),
-			STR => self.add(instruction),
-			TRAP => self.add(instruction),
+			ADD => self.add(instr),
+			AND => self.and(instr),
+			BR => self.br(instr),
+			JMP => self.jmp(instr),
+			JSR => self.jsr(instr),
+			LD => self.add(instr),
+			LDI => self.ldi(instr),
+			LDR => self.ldr(instr),
+			LEA => self.lea(instr),
+			NOT => self.not(instr),
+			RTI => self.rti(instr),
+			ST => self.st(instr),
+			STI => self.sti(instr),
+			STR => self.str(instr),
+			TRAP => self.trap(instr),
 
 			_ => return Err(())
 		}
@@ -100,148 +113,155 @@ impl LC3 {
 	}
 
 
-	fn add(&mut self, instruction: u16) -> () {
-		let dr: usize = Self::get_r0(instruction);
-		let a: u16 = self.reg[Self::get_r1(instruction)];
-		let b: u16 = if Self::get_imm5_flag(instruction) {Self::sign_extend(instruction & 0b11111, 5)} else {self.reg[Self::get_r2(instruction)]};
-		let sum: u16 = a + b;
+	fn add(&mut self, instr: u16) -> () {
+		let dr: usize = get_r0!(instr);
+		let a: u16 = self.reg[get_r1!(instr)];
+		let b: u16 = if get_imm5_flag!(instr) {sign_extend!(instr & 0x1F, 5)} else {self.reg[get_r2!(instr)]};
+		let value: u16 = a + b;
 
-		self.reg[dr] = sum;
-		self.set_cc(sum);
+		self.reg[dr] = value;
+		self.set_cc(value);
 	}
 
 
-	fn and(&mut self, instruction: u16) -> () {
-		let dr: usize = Self::get_r0(instruction);
-		let a: u16 = self.reg[Self::get_r1(instruction)];
-		let b: u16 = if Self::get_imm5_flag(instruction) {Self::sign_extend(instruction & 0b11111, 5)} else {self.reg[Self::get_r2(instruction)]};
-		let result: u16 = a & b;
+	fn and(&mut self, instr: u16) -> () {
+		let dr: usize = get_r0!(instr);
+		let a: u16 = self.reg[get_r1!(instr)];
+		let b: u16 = if get_imm5_flag!(instr) {sign_extend!(instr & 0x1F, 5)} else {self.reg[get_r2!(instr)]};
+		let value: u16 = a & b;
 
-		self.reg[dr] = result;
-		self.set_cc(result);
+		self.reg[dr] = value;
+		self.set_cc(value);
 	}
 
 
-	fn br(&mut self, instruction: u16) -> () {
-		let sign_flag: u16 = instruction >> 9 & 0b111;
-		if sign_flag & self.reg[CC] != 0 {
-			let pc_offset_9: u16 = Self::sign_extend(instruction & 0x1FF, 9);
-			self.reg[PC] += pc_offset_9;
+	fn br(&mut self, instr: u16) -> () {
+		let sign_flag: u16 = instr >> 9 & 0b111;
+		if sign_flag & self.cc != 0 {
+			let pc_offset_9: u16 = sign_extend!(instr & 0x1FF, 9);
+			self.pc += pc_offset_9;
 		}
 	}
 
 
-	fn jmp(&mut self, instruction: u16) -> () {
-		let br: usize = Self::get_r1(instruction);
-		self.reg[PC] = self.reg[br]
+	fn jmp(&mut self, instr: u16) -> () {
+		let br: usize = get_r1!(instr);
+		self.pc = self.reg[br]
 	}
 
 
-	fn jsr(&mut self, instruction: u16) -> () {
-		self.reg[R7] = self.reg[PC];
-		let jsr_flag: bool = instruction >> 11 & 1 == 1;
+	fn jsr(&mut self, instr: u16) -> () {
+		self.reg[7] = self.pc;
+		let jsr_flag: bool = instr >> 11 & 1 == 1;
 		if jsr_flag {
-			let pc_offset_11: u16 = Self::sign_extend(instruction & 0x7FF, 11);
-			self.reg[PC] += pc_offset_11;
+			let pc_offset_11: u16 = sign_extend!(instr & 0x7FF, 11);
+			self.pc += pc_offset_11;
 		}
 		else {
-			let br: usize = Self::get_r1(instruction);
-			self.reg[PC] = self.reg[br]
+			let br: usize = get_r1!(instr);
+			self.pc = self.reg[br]
 		}
 	}
 
 
-	fn ld(&mut self, instruction: u16) -> () {
-		let dr: usize = Self::get_r0(instruction);
-		let pc_offset_9: u16 = Self::sign_extend(instruction & 0x1FF, 9);
-		let result: u16 = self.read_mem(self.reg[PC] + pc_offset_9);
+	fn ld(&mut self, instr: u16) -> () {
+		let dr: usize = get_r0!(instr);
+		let pc_offset_9: u16 = sign_extend!(instr & 0x1FF, 9);
+		let address: usize = (self.pc + pc_offset_9) as usize;
+		let value: u16 = self.mem[address];
 
-		self.reg[dr] = result;
-		self.set_cc(result);
+		self.reg[dr] = value;
+		self.set_cc(value);
 	}
 
 
-	fn ldi(&mut self, instruction: u16) -> () {
-		let dr: usize = Self::get_r0(instruction);
-		let pc_offset_9: u16 = Self::sign_extend(instruction & 0x1FF, 9);
-		let result: u16 = self.read_mem(self.read_mem(self.reg[PC] + pc_offset_9));
+	fn ldi(&mut self, instr: u16) -> () {
+		let dr: usize = get_r0!(instr);
+		let pc_offset_9: u16 = sign_extend!(instr & 0x1FF, 9);
+		let address: usize = (self.pc + pc_offset_9) as usize;
+		let address_2: usize = self.mem[address] as usize;
+		let value: u16 = self.mem[address_2];
 
-		self.reg[dr] = result;
-		self.set_cc(result);
+		self.reg[dr] = value;
+		self.set_cc(value);
 	}
 
 
-	fn ldr(&mut self, instruction: u16) -> () {
-		let dr: usize = Self::get_r0(instruction);
-		let br: usize = Self::get_r1(instruction);
-		let pc_offset_6: u16 = Self::sign_extend(instruction & 0x3F, 6);
-		let result: u16 = self.read_mem(self.reg[br] + pc_offset_6);
+	fn ldr(&mut self, instr: u16) -> () {
+		let dr: usize = get_r0!(instr);
+		let br: usize = get_r1!(instr);
+		let pc_offset_6: u16 = sign_extend!(instr & 0x3F, 6);
+		let address: usize = (self.reg[br] + pc_offset_6) as usize;
+		let value: u16 = self.mem[address];
 
-		self.reg[dr] = result;
-		self.set_cc(result);
+		self.reg[dr] = value;
+		self.set_cc(value);
 	}
 
 
-	fn lea(&mut self, instruction: u16) -> () {
-		let dr: usize = Self::get_r0(instruction);
-		let pc_offset_9: u16 = Self::sign_extend(instruction & 0x1FF, 9);
-		let result: u16 = self.reg[PC] + pc_offset_9;
+	fn lea(&mut self, instr: u16) -> () {
+		let dr: usize = get_r0!(instr);
+		let pc_offset_9: u16 = sign_extend!(instr & 0x1FF, 9);
+		let address: u16 = self.pc + pc_offset_9;
 
-		self.reg[dr] = result;
-		self.set_cc(result);
+		self.reg[dr] = address;
+		self.set_cc(address);
 	}
 
 
-	fn not(&mut self, instruction: u16) -> () {
-		let dr: usize = Self::get_r0(instruction);
-		let sr: usize = Self::get_r1(instruction);
-		let result: u16 = !self.reg[sr];
+	fn not(&mut self, instr: u16) -> () {
+		let dr: usize = get_r0!(instr);
+		let sr: usize = get_r1!(instr);
+		let value: u16 = !self.reg[sr];
 
-		self.reg[dr] = result;
-		self.set_cc(result);
+		self.reg[dr] = value;
+		self.set_cc(value);
 	}
 
 
-	fn rti(&mut self, _instruction: u16) -> () {}
+	fn rti(&mut self, _instr: u16) -> () {}
+
+
+	fn st(&mut self, instr: u16) -> () {
+		let sr: usize = get_r0!(instr);
+		let pc_offset_9: u16 = sign_extend!(instr & 0x1FF, 9);
+		let address: usize = (self.pc + pc_offset_9) as usize;
+		self.mem[address] = self.reg[sr];
+	}
+
+
+	fn sti(&mut self, instr: u16) -> () {
+		let sr: usize = get_r0!(instr);
+		let pc_offset_9: u16 = sign_extend!(instr & 0x1FF, 9);
+		let address: usize = (self.pc + pc_offset_9) as usize;
+		let address_2: usize = self.mem[address] as usize;
+		self.mem[address_2] = self.reg[sr];
+	}
+
+
+	fn str(&mut self, instr: u16) -> () {
+		let sr: usize = get_r0!(instr);
+		let br: usize = get_r1!(instr);
+		let pc_offset_6: u16 = sign_extend!(instr & 0x3F, 6);
+		let address: usize = (self.reg[br] + pc_offset_6) as usize;
+		self.mem[address] = self.reg[sr];
+	}
+
+
+	fn trap(&mut self, instr: u16) -> () {
+
+	}
 
 
 	fn set_cc(&mut self, value: u16) -> () {
 		if value == 0 {
-			self.reg[CC] = Z;
+			self.cc = Z;
 		}
 		else if value >> 15 & 1 == 1 {
-			self.reg[CC] = N;
+			self.cc = N;
 		}
 		else {
-			self.reg[CC] = P;
+			self.cc = P;
 		}
-	}
-
-
-	fn get_r0(instruction: u16) -> usize {
-		return (instruction >> 9 & 0b111) as usize;
-	}
-
-
-	fn get_r1(instruction: u16) -> usize {
-		return (instruction >> 6 & 0b111) as usize;
-	}
-
-
-	fn get_r2(instruction: u16) -> usize {
-		return (instruction & 0b111) as usize;
-	}
-
-
-	fn get_imm5_flag(instruction: u16) -> bool {
-		return instruction >> 5 & 1 == 1;
-	}
-
-
-	fn sign_extend(a: u16, bit_count: u16) -> u16 {
-		if a >> (bit_count - 1) & 1 == 1 {
-			return a | (0xFFFF << bit_count);
-		}
-		return a & ((1 << bit_count) - 1);
 	}
 }
